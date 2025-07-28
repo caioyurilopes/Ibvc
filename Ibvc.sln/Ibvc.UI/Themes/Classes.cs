29a990efa4a46e1a1fe8d1a58b432907888f9099
Caio Lopes <caioyurilopes@gmail.com>
2025-07-27 23:38:17 -0300

    Começando UI

---
+++ Ibvc.sln/Ibvc.UI/Themes/Classes.cs
@@ -0,0 +1,7 @@
+namespace Ibvc.UI.Themes;
+
+public abstract class Classes
+{
+    public const string InputLogin = "block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6";
+    public const string BtnLogin = "flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600";
+}
---
+++ Ibvc.sln/Ibvc.UI/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./**/*.razor",
+      "./wwwroot/index.html",
+      "./**/*.cshtml"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
